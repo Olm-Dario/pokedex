730708571a25fb7028be4b60464ca227d2a5f64a
Dario <dariolmedo2@gmail.com>
2023-01-26 11:54:23 -0300

    Creacion de componentes para la navegacion

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,18 @@
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import Favorite from "../screens/Favorite";
+import Pokedex from "../screens/Pokedex";
+import Account from "../screens/Account";
+
+const Tab = createBottomTabNavigator();
+
+const Navigation = () => {
+  return(
+    <Tab.Navigator>
+      <Tab.Screen name="Favotire" component={Favorite} />
+      <Tab.Screen name="Pokedex" component={Pokedex} />
+      <Tab.Screen name="Account" component={Account} />
+    </Tab.Navigator>
+  );
+}
+
+export default Navigation;
---
+++ src/screens/Pokedex.js
@@ -0,0 +1,12 @@
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Pokedex = () => {
+  return(
+    <SafeAreaView>
+      <Text>Pokedex</Text>
+    </SafeAreaView>
+  );
+}
+
+export default Pokedex;
