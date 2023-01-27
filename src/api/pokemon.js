bae33596c8c520aee6314482e3e5ff098e7a58ac
Dario <dariolmedo2@gmail.com>
2023-01-27 11:38:35 -0300

    Obtener informacion detallada de los pokemons

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -9,4 +9,14 @@ export async function getPokemonsApi () {
   } catch (error) {
     throw error;
   }    
-}
+};
+
+export async function getPokemonDatailsByUrlApi (url) {
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import { View, Text } from "react-native";
-import { getPokemonsApi } from "../api/pokemon";
+import { getPokemonsApi, getPokemonDatailsByUrlApi } from "../api/pokemon";
 
 const Pokemon = () => {
-  
+  const [ pokemons, setPokemons ] = useState([]);
+
   useEffect(() => {
     ( async () => {
       await logPokemon();
@@ -13,7 +14,23 @@ const Pokemon = () => {
   const logPokemon = async () => {
     try {
       const response = await getPokemonsApi();
-      console.log(response);
+      
+      const pokemonArray = [];
+      for await (const pokemon of response.results){
+        const pokemonDetails = await getPokemonDatailsByUrlApi(pokemon.url);
+        
+        pokemonArray.push({
+          id: pokemonDetails.id,
+          name: pokemonDetails.name,
+          type: pokemonDetails.types[0].type.name,
+          order: pokemonDetails.order,
+          image: pokemonDetails.sprites.other['official-artwork'].front_default,
+        });
+      }
+
+      setPokemons([...pokemons, ...pokemonArray])
+      console.log(pokemons)
+
     } catch (error) {
       console.log(error);
     }
