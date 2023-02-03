1bfc7f4dbc1c5882f1dd64dd6f369f614a550876
Dario <dariolmedo2@gmail.com>
2023-02-03 10:34:38 -0300

    Implementando Formik y Yup

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,6 +1,19 @@
 import { StyleSheet, View, Text, TextInput, Button, Keyboard } from 'react-native';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
 
 const Loginform = () => {
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    validateOnChange: false,
+    onSubmit: (formData) => {
+      console.log(formData)
+    }
+  });
+
   return (
     <View>
       <Text style={styles.title}>Iniciar sesión</Text>
@@ -8,14 +21,20 @@ const Loginform = () => {
         placeholder='Nombre de usuario'
         style={styles.input}
         autoCapitalize='none'
+        value={formik.values.userName}
+        onChangeText={(text) => formik.setFieldValue('userName', text)}
       />
       <TextInput
         placeholder='Contraseña'
         style={styles.input}
         autoCapitalize='none'
         secureTextEntry={true}
+        value={formik.values.passWord}
+        onChangeText={(text) => formik.setFieldValue('passWord', text)}
       />
-      <Button title='Entrar' onPress={() => console.log("entrando")} />
+      <Button title='Entrar' onPress={formik.handleSubmit} />
+      <Text style={styles.error}>{formik.errors.userName}</Text>
+      <Text style={styles.error}>{formik.errors.passWord}</Text>
     </View>
   )
 };
@@ -34,7 +53,26 @@ const styles = StyleSheet.create({
     padding: 10,
     borderWidth: 1,
     borderRadius: 10,
+  },
+  error: {
+    textAlign: 'center',
+    color: '#f00',
+    marginTop: 20,
   }
 });
+
+const initialValues = () => {
+  return {
+    userName: "",
+    passWord: "",
+  }
+};
+
+const validationSchema = () => {
+  return {
+    userName: Yup.string().required("El usuario es obligatorio"),
+    passWord: Yup.string().required("La contraseña es obligatoria")
+  }
+};
 
 export default Loginform;
