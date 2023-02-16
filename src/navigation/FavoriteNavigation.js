47654a3f27fd440969398fc3c6fd95ed848c72a5
Dario <dariolmedo2@gmail.com>
2023-02-16 10:19:00 -0300

    Navegación hacia atrás en favoritos

--- src/navigation/FavoriteNavigation.js
+++ src/navigation/FavoriteNavigation.js
@@ -1,14 +1,27 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import Favorite from '../screens/Favorite';
+import Pokemon from '../screens/Pokemon';
 
 const Stack = createStackNavigator();
 
 const FavoriteNavigation = () => {
-  return(
+  return (
     <Stack.Navigator>
-      <Stack.Screen name='Favorite' component={Favorite} options={{
-        title: 'Favoritos'
-      }} />
+      <Stack.Screen
+        name='Favorite'
+        component={Favorite}
+        options={{
+          title: 'Favoritos'
+        }}
+      />
+      <Stack.Screen
+        name='Pokemon'
+        component={Pokemon}
+        options={{
+          title: '',
+          headerTransparent: true,
+        }}
+      />
     </Stack.Navigator>
   )
 };
