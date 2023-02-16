5b22b0da16beaf3cc4622e9b4816c53e8614990d
Dario <dariolmedo2@gmail.com>
2023-02-16 10:56:08 -0300

    Sincronizando información entre pantallas

--- src/components/Auth/UserData.js
+++ src/components/Auth/UserData.js
@@ -1,9 +1,26 @@
+import { useState, useCallback } from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import { size } from 'lodash';
 import useAuth from '../../hooks/useAuth';
+import { getPokemonFavoriteApi } from '../../api/favorite';
 
 const UserData = () => {
-
   const { auth, logout } = useAuth();
+  const [total, setTotal] = useState(0);
+
+  useFocusEffect(
+    useCallback(() => {
+      (async () => {
+        try {
+          const response = await getPokemonFavoriteApi();
+          setTotal(size(response));
+        } catch (error) {
+          setTotal(0);
+        }
+      })()
+    }, [])
+  );
 
   return (
     <View style={styles.content}>
@@ -18,7 +35,7 @@ const UserData = () => {
         <ItemMenu title='Nombre' text={`${auth.firstName} ${auth.lastName}`} />
         <ItemMenu title='UserName' text={auth.userName} />
         <ItemMenu title='Email' text={auth.email} />
-        <ItemMenu title='Total Favotitos' text={`0 pokemons`} />
+        <ItemMenu title='Total Favotitos' text={`${total} pokemons`} />
       </View>
       <View style={styles.btn}>
         <Button title='Desconectarse' onPress={logout} />
