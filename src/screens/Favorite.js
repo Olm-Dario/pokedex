1478e26f0682dfdfc316a09e9db47ea184336b8f
Dario <dariolmedo2@gmail.com>
2023-02-16 10:10:03 -0300

    Lista de Pokémon favoritos

--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,19 +1,49 @@
+import { useState, useEffect, useCallback } from 'react';
 import { Text, Button } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useFocusEffect } from '@react-navigation/native';
 import { getPokemonFavoriteApi } from '../api/favorite';
+import { getPokemonDetailsApi } from '../api/pokemon';
+import useAuth from '../hooks/useAuth';
+import PokemonList from '../components/PokemonList';
 
 const Favorite = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const { auth } = useAuth();
 
   const checkFavorite = async () => {
     const response = await getPokemonFavoriteApi();
     console.log(response);
   }
 
-  return (
-    <SafeAreaView>
-      <Text>Favorite</Text>
-      <Button title='Obtener Favoritos' onPress={checkFavorite} />
-    </SafeAreaView>
+  useFocusEffect(
+    useCallback(() => {
+      if (auth) {
+        (async () => {
+          const response = await getPokemonFavoriteApi();
+          const pokemonsArray = [];
+
+          for await (const id of response) {
+            const pokemonDetails = await getPokemonDetailsApi(id);
+
+            pokemonsArray.push({
+              id: pokemonDetails.id,
+              name: pokemonDetails.name,
+              type: pokemonDetails.types[0].type.name,
+              order: pokemonDetails.order,
+              image: pokemonDetails.sprites.other["official-artwork"].front_default,
+            });
+          }
+
+          setPokemons(pokemonsArray);
+        })()
+      }
+    }, [auth])
+  );
+
+  return !auth ? (
+    <Text>Usuario no logueado</Text>
+  ) : (
+    <PokemonList pokemons={pokemons} />
   );
 }
 
